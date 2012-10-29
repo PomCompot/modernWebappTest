0ef922eee64779b590b414e34ce8581eb46173b1
PomCompot <pomme_compote-github@yahoo.fr>
2012-10-29 16:58:46 +0100

    H2 qui marche vraiment

--- src/main/java/fr/pomcompot/sample/webapp/entities/Person.java
+++ src/main/java/fr/pomcompot/sample/webapp/entities/Person.java
@@ -1,5 +1,7 @@
 package fr.pomcompot.sample.webapp.entities;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -11,5 +13,9 @@ public class Person {
 	@Id
 	private String id;
 	
-    private String name;
+    private String firstname, lastname;
+    
+    private Date birthDate;
+    
+    private Gender gender;
 }
